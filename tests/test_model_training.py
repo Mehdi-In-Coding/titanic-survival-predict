167738f18bf2b8bfc82e4e0ce0384f0a792a0ca8
Mehdi <133195198+Mehdi-In-Coding@users.noreply.github.com>
2025-01-30 15:17:42 +0100

    Update test_model_training.py

--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -1,9 +1,14 @@
 import os
-from src.model_training import train_models
+import pytest
 import pandas as pd
+from src.model_training import train_models
+from src.data_preprocessing import preprocess_data
+
 
 def test_train_models():
-    train_df = pd.read_csv("data/train.csv")
-    train_models(train_df)
-    assert os.path.exists("models/logreg.pkl"), "Modèle logreg non sauvegardé"
-    assert os.path.exists("models/random_forest.pkl"), "Modèle random forest non sauvegardé"
+    train_df, _ = preprocess_data("data/train.csv", "data/test.csv")
+
+    models = train_models(train_df, model_dir="models_test")
+    
+    assert os.path.exists("models_test/logreg.pkl"), "Modèle logreg non sauvegardé"
+    assert os.path.exists("models_test/random_forest.pkl"), "Modèle RandomForest non sauvegardé"
